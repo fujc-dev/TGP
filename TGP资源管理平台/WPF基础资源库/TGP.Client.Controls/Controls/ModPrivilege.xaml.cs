99777ddef5a2411c6746cf76772a78485908e2c9
328969111@qq.com <fujun0824>
2017-04-05 20:49:49 +0800

    完善控件

--- TGP资源管理平台/WPF基础资源库/TGP.Client.Controls/Controls/ModPrivilege.xaml.cs
+++ TGP资源管理平台/WPF基础资源库/TGP.Client.Controls/Controls/ModPrivilege.xaml.cs
@@ -21,7 +21,53 @@ namespace TGP.UI.Core.Controls
 	{
 		public ModPrivilege()
 		{
-			
+
 		}
+
+
+
+		public String ModTitle
+		{
+			get { return (String)GetValue(ModTitleProperty); }
+			set { SetValue(ModTitleProperty, value); }
+		}
+		public static readonly DependencyProperty ModTitleProperty = DependencyProperty.Register("ModTitle", typeof(String), typeof(ModPrivilege), new PropertyMetadata(""));
+
+
+
+		public String ModContent
+		{
+			get { return (String)GetValue(ModContentProperty); }
+			set { SetValue(ModContentProperty, value); }
+		}
+
+		public static readonly DependencyProperty ModContentProperty = DependencyProperty.Register("ModContent", typeof(String), typeof(ModPrivilege), new PropertyMetadata(""));
+
+
+
+
+		public ImageSource OwnerIcon
+		{
+			get { return (ImageSource)GetValue(OwnerIconProperty); }
+			set { SetValue(OwnerIconProperty, value); }
+		}
+
+		public static readonly DependencyProperty OwnerIconProperty = DependencyProperty.Register("OwnerIcon", typeof(ImageSource), typeof(ModPrivilege), new PropertyMetadata(null));
+
+
+
+
+		public ImageSource ChildIcon
+		{
+			get { return (ImageSource)GetValue(ChildIconProperty); }
+			set { SetValue(ChildIconProperty, value); }
+		}
+
+		public static readonly DependencyProperty ChildIconProperty = DependencyProperty.Register("ChildIcon", typeof(ImageSource), typeof(ModPrivilege), new PropertyMetadata(null));
+
+
+
+
+
 	}
 }
