10d2ce3bcd9ca58ae2390156a3fde6047544d6cf
328969111@qq.com <fujun0824>
2017-04-07 22:15:29 +0800

    TGP.UI

---
+++ TGP资源管理平台/WPF基础资源库/TGP.WindowDemo/GHeaderEx.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TGP.WindowDemo
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public partial class GHeaderEx : Control
+    {
+        public GHeaderEx()
+        {
+            this.ClickCommand = new RoutedUICommand();
+            this.BindCommand(ClickCommand, OnClickCommand);
+
+        }
+
+        public ICommand ClickCommand { get; protected set; }  //窗体拖动
+
+        private void OnClickCommand(object sender, ExecutedRoutedEventArgs e)
+        {
+            
+            MessageBox.Show(e.Parameter.ToString());
+        }
+    }
+}
