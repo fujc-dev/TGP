0cae3f103198b734426af5aae514c5041d0ef2ab
dane55 <328969111@qq.com>
2017-09-26 15:19:12 +0800

    TGP

--- TGP资源管理平台/TGP.Win3DDemo/Window1.xaml.cs
+++ TGP资源管理平台/TGP.Win3DDemo/Window1.xaml.cs
@@ -10,18 +10,17 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace TGP.Win3DDemo
 {
     /// <summary>
-    /// MainWindow.xaml 的交互逻辑
+    /// Window1.xaml 的交互逻辑
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class Window1 : Window
     {
-        public MainWindow()
-        { 
+        public Window1()
+        {
             InitializeComponent();
         }
     }
