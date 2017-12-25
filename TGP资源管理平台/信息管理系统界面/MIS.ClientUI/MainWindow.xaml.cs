31b28ae0b548b757ec5503cb6595488c70bd984e
dane55 <328969111@qq.com>
2017-12-25 16:47:15 +0800

    TGP

--- TGP资源管理平台/信息管理系统界面/MIS.ClientUI/MainWindow.xaml.cs
+++ TGP资源管理平台/信息管理系统界面/MIS.ClientUI/MainWindow.xaml.cs
@@ -23,6 +23,21 @@ namespace MIS.ClientUI
         public MainWindow()
         {
             InitializeComponent();
+            this.Loaded += MainWindow_Loaded;
+        }
+
+        void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            // 设置全屏    
+            this.WindowState = System.Windows.WindowState.Normal;
+            this.WindowStyle = System.Windows.WindowStyle.None;
+            this.ResizeMode = System.Windows.ResizeMode.NoResize;
+            this.Topmost = true;
+
+            this.Left = 0.0;
+            this.Top = 0.0;
+            this.Width = System.Windows.SystemParameters.PrimaryScreenWidth;
+            this.Height = System.Windows.SystemParameters.PrimaryScreenHeight;   
         }
     }
 }
